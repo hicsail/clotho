7f73bad977b93162f4d014493a8521536b4341e7
jgyou-bu <jgyou@bu.edu>
2017-05-30 13:40:47 -0400

    Initial version of sequence.

---
+++ server/models/sequence.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const Joi = require('joi');
+const MongoModels = require('mongo-models');
+const Annotation = require('./annotation');
+const Person = require('./person');
+
+class Sequence extends MongoModels {
+    
+    static create(name, sequence, author, callback) {
+        Sequence.create(name, null, sequence, author, callback);
+    }
+
+    static create(name, description, sequence, author, callback) {
+        const document = {
+            name: name,
+            description: description,
+            sequence: sequence,
+            author: author
+        };
+
+    this.insertOne(document, (err, docs) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, docs[0]);
+    });
+    }
+
+    // Should these be moved to Annotation file?
+    createAnnotation(name, start, end, isForwardStrand, author) {
+        createAnnotation(name, null, start, end, isForwardStrand, author);
+    }
+    
+    createAnnotation(name, description, start, end, isForwardStrand, author) {
+
+    }
+
+    addAnnotation(annotation) {
+
+    }
+
+    // Original Java.
+    /*
+      public Annotation createAnnotation(String name, int start, int end, boolean isForwardStrand,
+            Person author) {
+        Annotation annotation = new Annotation(name, start, end, isForwardStrand, author);
+        addAnnotation(annotation);
+        return annotation;
+    }
+
+    public Annotation createAnnotation(String name, String description, int start, int end,
+            boolean isForwardStrand, Person author) {
+        Annotation annotation = new Annotation(name, description, start, end, isForwardStrand, author);
+        addAnnotation(annotation);
+        return annotation;
+    }
+    
+    public void addAnnotation(Annotation annotation) {
+        if (annotations == null) {
+            annotations = new HashSet<Annotation>();
+        }
+        annotations.add(annotation);
+    }
+
+    */
+    
+
+}
+
+Sequence.collection = 'sequences';
+
+Sequence.schema = Joi.object().keys({
+  _id: Joi.object(),
+  name: Joi.string().required(),
+  description: Joi.string().optional(),
+  sequence: Joi.string().required().regex("[ATUCGRYKMSWBDHVN]*", "nucleotide sequence"), // Case-insensitive.
+  author: Person.schema,
+  annotations: Joi.array().items(Annotation.schema),
+  parentSequence: Sequence.schema
+});
+
+
+// Needs to be changed.
+Sequence.indexes = = [
+  {key: {'name': 1}}
+];
+
+module.exports = Sequence;
