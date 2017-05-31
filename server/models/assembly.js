59ad71a3eff91e20730718f66c634b0ece7e1558
jgyou-bu <jgyou@bu.edu>
2017-05-31 16:57:52 -0400

    Add initial version of Assembly.

---
+++ server/models/assembly.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const Joi = require('joi');
+const MongoModels = require('mongo-models');
+const Part = require('part');
+
+class Assembly extends MongoModels {
+  static create(callback) {
+    const document = {
+      userId: userId,
+      contactId: contactId,
+      direction: direction,
+      timestamp: timestamp
+    };
+
+    this.insertOne(document, (err, docs) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, docs[0]);
+    });
+  }
+}
+
+// public Assembly createSubAssembly() {
+//   Assembly subAssembly = new Assembly();
+//   addSubAssembly(subAssembly);
+//   return subAssembly;
+// }
+//
+// public void addPart(Part part) {
+//   if (parts == null) {
+//     parts = new ArrayList<Part>();
+//   }
+//   parts.add(part);
+// }
+//
+// public void addSubAssembly(Assembly subAssembly) {
+//   if (subAssemblies == null) {
+//     subAssemblies = new ArrayList<Assembly>();
+//   }
+//   subAssemblies.add(subAssembly);
+// }
+
+
+Assembly.collection = 'assemblies';
+
+Assembly.schema = Joi.object().keys({
+  _id: Joi.object(),
+  parts: Joi.array().items(Part.schema),
+  subAssemblies: Joi.array().items(Assembly.schema)
+});
+
+// @Getter
+// @Setter
+// @ReferenceCollection
+// protected List<Part> parts;
+//
+// @Getter
+// protected List<Assembly> subAssemblies;
+
+Assembly.indexes = [];
+
+module.exports = Assembly;
