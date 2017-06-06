d9918493e6a80d86ccb2a19bc5629911f618d618
jgyou-bu <jgyou@bu.edu>
2017-06-06 12:13:02 -0400

    Add sample data model.

---
+++ server/models/sample-data.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const Joi = require('joi');
+const MongoModels = require('mongo-models');
+
+class SampleData extends MongoModels {
+  static create(name, description, sampleId, instrumentId, responseVariableIds, userId, callback) {
+
+    const document = {
+      name: name,
+      description: description,
+      sampleId: sampleId,
+      instrumentId: instrumentId,
+      responseVariableIds: responseVariableIds,
+      userId: userId
+    };
+
+    this.insertOne(document, (err, docs) => {
+
+      if (err) {
+        return callback(err);
+      }
+      callback(null, docs[0]);
+    });
+  }
+}
+
+
+SampleData.collection = 'sampleData';
+
+SampleData.schema = Joi.object().keys({
+  _id: Joi.object(),
+  name: Joi.string().required(),
+  description: Joi.string(),
+  sampleId: Joi.string().required(),
+  instrumentId: Joi.string().required(),
+  responseVariableIds: Joi.array().items(Joi.string()),
+  userId: Joi.string().required(),
+  parameterIds: Joi.array().items(Joi.string())
+});
+
+SampleData.indexes = [
+  {key: {userId: 1}}
+];
+
+module.exports = SampleData;
+
+
+//
+// public Parameter createParameter(double value, Variable variable) {
+//   Parameter parameter = new Parameter(value, variable);
+//   addParameter(parameter);
+//   return parameter;
+// }
+//
+// public void addParameter(Parameter parameter) {
+//   if (parameters == null) {
+//     parameters = new HashSet<Parameter>();
+//   }
+//   parameters.add(parameter);
+// }
