6aa8eb337bf4c7fea0d179665fca693268114b6d
Greg Frasco <frascog@bu.edu>
2017-06-09 17:11:27 -0400

    Added test cases for bio design, influences, and parameter apis

---
+++ test/server/api/influences.js
@@ -0,0 +1,312 @@
+'use strict';
+const AuthPlugin = require('../../../server/auth');
+const AuthenticatedUser = require('../fixtures/credentials-admin');
+const Code = require('code');
+const Config = require('../../../config');
+const Hapi = require('hapi');
+const HapiAuthBasic = require('hapi-auth-basic');
+const Lab = require('lab');
+const MakeMockModel = require('../fixtures/make-mock-model');
+const Manifest = require('../../../manifest');
+const Path = require('path');
+const Proxyquire = require('proxyquire');
+const InfluencePlugin = require('../../../server/api/influences');
+
+const lab = exports.lab = Lab.script();
+let request;
+let server;
+let stub;
+
+
+lab.before((done) => {
+
+  stub = {
+    Influence: MakeMockModel()
+  };
+
+  const proxy = {};
+  proxy[Path.join(process.cwd(), './server/models/influence')] = stub.Influence;
+
+  const ModelsPlugin = {
+    register: Proxyquire('hapi-mongo-models', proxy),
+    options: Manifest.get('/registrations').filter((reg) => {
+
+      if (reg.plugin &&
+        reg.plugin.register &&
+        reg.plugin.register === 'hapi-mongo-models') {
+
+        return true;
+      }
+
+      return false;
+    })[0].plugin.options
+  };
+
+  const plugins = [HapiAuthBasic, ModelsPlugin, AuthPlugin, InfluencePlugin];
+  server = new Hapi.Server();
+  server.connection({port: Config.get('/port/web')});
+  server.register(plugins, (err) => {
+
+    if (err) {
+      return done(err);
+    }
+
+    server.initialize(done);
+  });
+});
+
+
+lab.after((done) => {
+
+  server.plugins['hapi-mongo-models'].MongoModels.disconnect();
+
+  done();
+});
+
+
+lab.experiment('Influence Plugin Result List', () => {
+
+  lab.beforeEach((done) => {
+
+    request = {
+      method: 'GET',
+      url: '/influence',
+      credentials: AuthenticatedUser
+    };
+
+    done();
+  });
+
+
+  lab.test('it returns an error when paged find fails', (done) => {
+
+    stub.Influence.pagedFind = function () {
+
+      const args = Array.prototype.slice.call(arguments);
+      const callback = args.pop();
+
+      callback(Error('find failed'));
+    };
+
+    server.inject(request, (response) => {
+
+      Code.expect(response.statusCode).to.equal(500);
+
+      done();
+    });
+  });
+
+
+  lab.test('it returns an array of documents successfully', (done) => {
+
+    stub.Influence.pagedFind = function () {
+
+      const args = Array.prototype.slice.call(arguments);
+      const callback = args.pop();
+
+      callback(null, {data: [{}, {}, {}]});
+    };
+
+    server.inject(request, (response) => {
+
+      Code.expect(response.statusCode).to.equal(200);
+      Code.expect(response.result.data).to.be.an.array();
+      Code.expect(response.result.data[0]).to.be.an.object();
+
+      done();
+    });
+  });
+});
+
+
+lab.experiment('Influence Plugin Read', () => {
+
+  lab.beforeEach((done) => {
+
+    request = {
+      method: 'GET',
+      url: '/influence/93EP150D35',
+      credentials: AuthenticatedUser
+    };
+
+    done();
+  });
+
+
+  lab.test('it returns an error when find by id fails', (done) => {
+
+    stub.Influence.findById = function (id, callback) {
+
+      callback(Error('find by id failed'));
+    };
+
+    server.inject(request, (response) => {
+
+      Code.expect(response.statusCode).to.equal(500);
+
+      done();
+    });
+  });
+
+
+  lab.test('it returns a not found when find by id misses', (done) => {
+
+    stub.Influence.findById = function (id, callback) {
+
+      callback();
+    };
+
+    server.inject(request, (response) => {
+
+      Code.expect(response.statusCode).to.equal(404);
+      Code.expect(response.result.message).to.match(/document not found/i);
+
+      done();
+    });
+  });
+
+
+  lab.test('it returns a document successfully', (done) => {
+
+    stub.Influence.findById = function (id, callback) {
+
+      callback(null, {_id: '93EP150D35'});
+    };
+
+    server.inject(request, (response) => {
+
+      Code.expect(response.statusCode).to.equal(200);
+      Code.expect(response.result).to.be.an.object();
+
+      done();
+    });
+  });
+});
+
+
+lab.experiment('Influence Plugin Delete', () => {
+
+  lab.beforeEach((done) => {
+
+    request = {
+      method: 'DELETE',
+      url: '/influence/93EP150D35',
+      credentials: AuthenticatedUser
+    };
+
+    done();
+  });
+
+
+  lab.test('it returns an error when delete by id fails', (done) => {
+
+    stub.Influence.findByIdAndDelete = function (id, callback) {
+
+      callback(Error('delete by id failed'));
+    };
+
+    server.inject(request, (response) => {
+
+      Code.expect(response.statusCode).to.equal(500);
+
+      done();
+    });
+  });
+
+
+  lab.test('it returns a not found when delete by id misses', (done) => {
+
+    stub.Influence.findByIdAndDelete = function (id, callback) {
+
+      callback(null, undefined);
+    };
+
+    server.inject(request, (response) => {
+
+      Code.expect(response.statusCode).to.equal(404);
+      Code.expect(response.result.message).to.match(/document not found/i);
+
+      done();
+    });
+  });
+
+
+  lab.test('it deletes a document successfully', (done) => {
+
+    stub.Influence.findByIdAndDelete = function (id, callback) {
+
+      callback(null, 1);
+    };
+
+    server.inject(request, (response) => {
+
+      Code.expect(response.statusCode).to.equal(200);
+      Code.expect(response.result.message).to.match(/success/i);
+
+      done();
+    });
+  });
+});
+/*
+lab.experiment('Influence Plugin Create', () => {
+
+  lab.beforeEach((done) => {
+
+    request = {
+      method: 'POST',
+      url: '/influence',
+      payload: {
+        sequenceId: 'seq1',
+        name: 'sequence1',
+        start: 1,
+        end: 2,
+        isForwardStrand: true
+      },
+      credentials: AuthenticatedUser
+    };
+
+    done();
+  });
+
+  lab.test('it returns an error when create fails', (done) => {
+
+    stub.Influence.findOne = function (conditions, callback) {
+
+      callback();
+    };
+
+    stub.Influence.create = function (sequenceId,name,description,start,end,isForwardStrand,userId,callback) {
+
+      callback(Error('create failed'));
+    };
+
+    server.inject(request, (response) => {
+
+      Code.expect(response.statusCode).to.equal(500);
+
+      done();
+    });
+  });
+
+  lab.test('it creates a document successfully', (done) => {
+
+    stub.Influence.findOne = function (conditions, callback) {
+
+      callback();
+    };
+
+    stub.Influence.create = function (name,description,sequence,isLinear,isSingleStranded,featureId,userId,callback) {
+
+      callback(null, {});
+    };
+
+    server.inject(request, (response) => {
+
+      Code.expect(response.statusCode).to.equal(200);
+      Code.expect(response.result).to.be.an.object();
+
+      done();
+    });
+  });
+});
+*/
