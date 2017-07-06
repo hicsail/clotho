09fc5674242a7a7c3660aaeae1c9e983f1b1175a
Ibrahim Shaikh <ibrahims123457@yahoo.com>
2017-07-06 13:35:36 -0400

    Complete blast api

--- server/api/blasts.js
+++ server/api/blasts.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Joi = require('joi');
-const { spawn } = require('child_process');
+const { execFile } = require('child_process');
 const fs = require('fs');
 
 const internals = {};
@@ -17,24 +17,53 @@ internals.applyRoutes = function (server, next) {
       },
       validate: {
         payload: {
-          sequenceId: Joi.string().required()
+          name: Joi.string().required(),
+          displayId: Joi.string().optional(),
+          role: Joi.string().valid('BARCODE', 'CDS', 'DEGRADATION_TAG', 'GENE', 'LOCALIZATION_TAG', 'OPERATOR', 'PROMOTER', 'SCAR', 'SPACER', 'RBS', 'RIBOZYME', 'TERMINATOR'),
+          parameters: Joi.array().items(Joi.object()).optional(), // assumed to be of the format (value, variable)
+          sequence: Joi.string().regex(/^[ATUCGRYKMSWBDHVNatucgrykmswbdhvn]+$/, 'DNA sequence').insensitive()
         }
       }
     },
 
     handler: function (request, reply) {
-      const mkdir = spawn('mkdir', ['-p', './blast/test1']);
-
-      mkdir.stderr.on('data', (data) => {
-        console.log(`stderr: ${data}`);
+      const payload = request.payload;
+      var sequence = $.post('/api/part', payload, function (data) {
+        return {
+          seq: data[0].parts[0].sequences[0].sequence,
+          seqId: data[0].parts[0].sequences[0]._id,
+          name: data[0].parts[0].sequences[0].name
+        };
       });
 
-      // Create and write to file.
-      var fileContent = '> Sequence Name-SequenceID\nTAGDTAGDTdnalettersTSGAGTA';
-      fs.writeFile('blast/test1/tesfile.fasta', fileContent, function (err) {
-
+      const directory = './blast/temp' + sequence.seqId;
+      const mkdir = execFile('mkdir', ['-p', directory], (err, stdout, stderr) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Successfully created temp directory.');
+        }
       });
 
+      var fileContent = '> ' + sequence.name + '-' + sequence.seqId + '\n' + sequence.seq;  // Content of the fasta file
+      var fileLocation = directory + '/' + sequence.name + '.fasta';  // File name and location
+      fs.writeFile(fileLocation, fileContent, function (err) {  // Write the fasta file
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('FASTA file was saved.');
+        }
+      });
+
+      reply.file(fileLocation); // Reply with the fasta file
+
+      const deleteDir = execFile('rm', ['-f', directory], (err, stdout, stderr) => {
+          if (err){
+            console.log(err);
+          } else {
+            console.log('Temp directory was successfully deleted');
+          }
+      });
     }
   });
 };
