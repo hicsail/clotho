52b5a0a71e4350bee6f8829a785e94e2dc2306ed
Ibrahim Shaikh <ibrahims123457@yahoo.com>
2017-07-10 18:34:41 -0400

    Api accounts for whether or not the part is in the db

--- server/api/blasts.js
+++ server/api/blasts.js
@@ -3,7 +3,7 @@
 const Joi = require('joi');
 const { execFile } = require('child_process');
 const fs = require('fs');
-const wreck = require('wreck');
+const Hapi = require('hapi');
 
 const internals = {};
 
@@ -18,33 +18,58 @@ internals.applyRoutes = function (server, next) {
       },
       validate: {
         payload: {
-          name: Joi.string().required(),
-          displayId: Joi.string().optional(),
+          sort: Joi.string().default('_id'),
+          limit: Joi.number().default(20),
+          page: Joi.number().default(1),
+          name: Joi.string(),
+          displayId: Joi.string(),
           role: Joi.string().valid('BARCODE', 'CDS', 'DEGRADATION_TAG', 'GENE', 'LOCALIZATION_TAG', 'OPERATOR', 'PROMOTER', 'SCAR', 'SPACER', 'RBS', 'RIBOZYME', 'TERMINATOR'),
-          parameters: Joi.array().items(Joi.object()).optional(), // assumed to be of the format (value, variable)
-          sequence: Joi.string().regex(/^[ATUCGRYKMSWBDHVNatucgrykmswbdhvn]+$/, 'DNA sequence').insensitive()
+          sequence: Joi.string().regex(/^[ATUCGRYKMSWBDHVNatucgrykmswbdhvn]+$/, 'DNA sequence').insensitive(),
+          parameters: Joi.array().items(
+            Joi.object().keys({
+              name: Joi.string().optional(),
+              units: Joi.string(), // These should be updated.
+              value: Joi.number(),
+              variable: Joi.string()
+            })
+          ).optional(),
+          userSpace: Joi.boolean().default(false)
         }
       }
     },
 
     handler: function (request, reply) {
-      const payload = request.payload;
-      var sequence = wreck.post('./api/part', {payload: payload}, (err, res, payload) => {
-        return {
-          seq: res[0].parts[0].sequences[0].sequence,
-          seqId: res[0].parts[0].sequences[0]._id,
-          name: res[0].parts[0].sequences[0].name
-        };
-      });
-      // var sequence = $.post('/api/part', payload, function (data) {
-      //   return {
-      //     seq: data[0].parts[0].sequences[0].sequence,
-      //     seqId: data[0].parts[0].sequences[0]._id,
-      //     name: data[0].parts[0].sequences[0].name
-      //   };
-      // });
 
-      const directory = './blast/temp' + sequence.seqId;
+      var req = {
+        method: 'PUT',
+        url: '/api/part',
+        payload: request.payload
+      };
+
+      var i;
+      var sequence = [];
+      var response = server.inject(req, (response) => {
+        try{  // If there is an error then there was no part in the db that matched the sequence
+          for (i = 0; i < response[0].parts[0].sequences.length(); i++) {
+            sequence.push({seq: response[0].parts[0].sequences[i].sequence,
+              seqId: response[0].parts[0].sequences[i]._id,
+              name: response[0].parts[0].sequences[i].name
+            });
+          }
+        } catch(e) {
+          return e;
+        }
+      });
+
+
+      // var sequence = {
+      //   seq: request.payload.sequence,
+      //   seqId: request.payload.displayId,
+      //   name: request.payload.name
+      // };
+
+      const randomNum = Math.floor(Math.random()*1000)+1; // Random number generator to create random temp directory name
+      const directory = './blast/temp' + randomNum.toString();
       const mkdir = execFile('mkdir', ['-p', directory], (err, stdout, stderr) => {
         if (err) {
           console.log(err);
@@ -53,7 +78,12 @@ internals.applyRoutes = function (server, next) {
         }
       });
 
-      var fileContent = '> ' + sequence.name + '-' + sequence.seqId + '\n' + sequence.seq;  // Content of the fasta file
+      var fileContent = '';
+      for(i = 0; i < sequence.length; i++) {
+        fileContent += '> ' + sequence[i].name + '-' + sequence[i].seqId + '\n' + sequence[i].seq + '\n\n';
+      }
+
+      // var fileContent = '> ' + sequence.name + '-' + sequence.seqId + '\n' + sequence.seq;  // Content of the fasta file
       var fileLocation = directory + '/' + sequence.name + '.fasta';  // File name and location
       fs.writeFile(fileLocation, fileContent, function (err) {  // Write the fasta file
         if (err) {
