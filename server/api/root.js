f9e3dc3ec7269fe8add70c728f28172d128bbf8c
Ibrahim Shaikh <ibrahims123457@yahoo.com>
2017-07-21 15:14:44 -0400

    Add api

--- server/api/root.js
+++ server/api/root.js
@@ -8,84 +8,19 @@ const internals = {};
 
 internals.applyRoutes = function (server, next) {
 
-  const User = server.plugins['hapi-mongo-models'].User;
 
-  server.route({
-    method: 'POST',
-    path: '/setup',
-    config: {
-      auth: {
-        mode: 'try',
-        strategy: 'session'
-      },
-      validate: {
-        payload: {
-          email: Joi.string().email().lowercase().required(),
-          password: password: Joi.string().required()
-        }
-      },
-      plugins: {
-        'hapi-auth-cookie': {
-          redirectTo: false
-        }
-      },
-    },
-    handler: function (request, reply) {
 
-      Async.auto({
-        createUser: function (done) {
-          const email = request.payload.email;
-          const password = request.payload.password;
+  next();
+};
 
-          User.create('root', password, email, 'root', done);
-        },
-        account: ['user', function (results, done) {
+exports.register = function (server, options, next) {
 
-          Account.create('root', done);
-        }],
-        linkUser: ['account', function (results, done) {
+  server.dependency(['auth', 'hapi-mongo-models'], internals.applyRoutes);
 
-          const id = results.account._id.toString();
-          const update = {
-            $set: {
-              user: {
-                id: results.user._id.toString(),
-                name: results.user.username
-              }
-            }
-          };
+  next();
+};
 
-          Account.findByIdAndUpdate(id, update, done);
-        }],
-        linkAccount: ['account', function (results, done) {
 
-          const id = results.user._id.toString();
-          const update = {
-            $set: {
-              roles: {
-                account: {
-                  id: results.account._id.toString(),
-                  name: results.account.name.first + ' ' + results.account.name.last
-                }
-              }
-            }
-          };
-
-          User.findByIdAndUpdate(id, update, done);
-        }],
-        update: ['linkUser', 'linkAccount', function(results, done) {
-          const id = results.user.id.toString();
-          const update = {
-            $set: {
-              user: {
-                __id: 0000
-              }
-            }
-          };
-          User.findByIdAndUpdate(id,update, done);
-        }]
-      });
-
-    }
-  });
+exports.register.attributes = {
+  name: 'root'
 };
